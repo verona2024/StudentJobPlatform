74485106a41408130e6474df3343b503ab7feb51
verona2024 <verona.ademaj@umib.net>
2026-03-23 23:49:27 +0100

    Refactor Program.cs to include FileRepository class

--- StudentJobPlatform/Program.cs
+++ StudentJobPlatform/Program.cs
@@ -1,22 +1,84 @@
-using StudentJobPlatform.Data;
-using StudentJobPlatform.Models;
-using StudentJobPlatform.Services;
-using StudentJobPlatform.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-var userRepo = new FileRepository<User>(@"..\..\..\Files\users.csv");
-var jobRepo = new FileRepository<Job>(@"..\..\..\Files\jobs.csv");
-var applicationRepo = new FileRepository<Application>(@"..\..\..\Files\applications.csv");
-
-if (!jobRepo.GetAll().Any())
+namespace StudentJobPlatform.Data
 {
-    jobRepo.Add(new Job(1, "Frontend Intern", "Punë part-time për student", "IT", "Mitrovicë", "20 orë/javë", 250, 1));
-    jobRepo.Add(new Job(2, "Marketing Assistant", "Punë për studentë", "Marketing", "Prishtinë", "15 orë/javë", 200, 2));
-    jobRepo.Save();
+    public class FileRepository<T> : IRepository<T>
+    {
+        private readonly string _filePath;
+        private readonly List<T> _items;
+
+        public FileRepository(string filePath)
+        {
+            _filePath = filePath;
+            _items = LoadFromFile();
+        }
+
+        public List<T> GetAll()
+        {
+            return _items;
+        }
+
+        public T? GetById(int id)
+        {
+            return _items.FirstOrDefault(item =>
+            {
+                var property = item!.GetType().GetProperty("Id");
+                if (property == null) return false;
+
+                int value = (int)property.GetValue(item)!;
+                return value == id;
+            });
+        }
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void Save()
+        {
+            var lines = _items.Select(item => item!.ToString()).ToList();
+            File.WriteAllLines(_filePath, lines);
+        }
+
+        private List<T> LoadFromFile()
+        {
+            var items = new List<T>();
+
+            if (!File.Exists(_filePath))
+                return items;
+
+            var lines = File.ReadAllLines(_filePath);
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                if (typeof(T).Name == "Job")
+                {
+                    var parts = line.Split(',');
+
+                    var job = (T)Activator.CreateInstance(
+                        typeof(T),
+                        int.Parse(parts[0]),
+                        parts[1],
+                        parts[2],
+                        parts[3],
+                        parts[4],
+                        parts[5],
+                        decimal.Parse(parts[6]),
+                        int.Parse(parts[7])
+                    )!;
+
+                    items.Add(job);
+                }
+            }
+
+            return items;
+        }
+    }
 }
-
-var menu = new MenuManager(
-    new JobService(jobRepo),
-    new ApplicationService(applicationRepo, jobRepo)
-);
-
-menu.Start();
