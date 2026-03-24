7acda5dd80067672bbaa788412be81d0f6f38b21
verona2024 <verona.ademaj@umib.net>
2026-03-24 23:15:18 +0100

    Update User.cs
    Added properties for Major, Skills, and Availability to the User class.

--- StudentJobPlatform/Models/User.cs
+++ StudentJobPlatform/Models/User.cs
@@ -1,4 +1,4 @@
-﻿namespace StudentJobPlatform.Models
+namespace StudentJobPlatform.Models
 {
     public class User
     {
@@ -7,11 +7,17 @@
         private string _email;
         private string _password;
         private string _role;
+        private string _major;
+        private string _skills;
+        private string _availability;
 
         public int Id => _id;
         public string Name => _name;
         public string Email => _email;
         public string Role => _role;
+        public string Major => _major;
+        public string Skills => _skills;
+        public string Availability => _availability;
 
         public User(int id, string name, string email, string password, string role)
         {
@@ -20,11 +26,19 @@
             _email = email;
             _password = password;
             _role = role;
+            _major = "";
+            _skills = "";
+            _availability = "";
         }
 
         public bool CheckPassword(string password)
         {
             return _password == password;
+        }
+
+        public override string ToString()
+        {
+            return $"{_id},{_name},{_email},{_password},{_role},{_major},{_skills},{_availability}";
         }
     }
 }
