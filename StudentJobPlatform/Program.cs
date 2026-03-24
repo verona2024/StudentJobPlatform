71abc44538e3baf105121223e37fd4f17ac44f52
verona2024 <verona.ademaj@umib.net>
2026-03-24 23:21:51 +0100

    Refactor Program.cs to improve structure and services

--- StudentJobPlatform/Program.cs
+++ StudentJobPlatform/Program.cs
@@ -1,10 +1,10 @@
+namespace StudentJobPlatform;
 using StudentJobPlatform.Data;
 using StudentJobPlatform.Models;
 using StudentJobPlatform.Services;
 using StudentJobPlatform.UI;
-
-var userRepo = new FileRepository<User>(@"..\..\..\Files\users.csv");
-var jobRepo = new FileRepository<Job>(@"..\..\..\Files\jobs.csv");
-var applicationRepo = new FileRepository<Application>(@"..\..\..\Files\applications.csv");
-DataSeeder.SeedJobs(jobRepo);
-new MenuManager(new JobService(jobRepo), new ApplicationService(applicationRepo, jobRepo)).Start();
+public class Program
+{  public static void Main()
+    {   var userRepo = new FileRepository<User>(@"..\..\..\Files\users.csv"); var jobRepo = new FileRepository<Job>(@"..\..\..\Files\jobs.csv"); var appRepo = new FileRepository<Application>(@"..\..\..\Files\applications.csv");
+        AppServices.JobService = new JobService(jobRepo); AppServices.ApplicationService = new ApplicationService(appRepo, jobRepo); AppServices.AuthService = new AuthService(userRepo);
+        DataSeeder.SeedJobs(jobRepo); new AuthMenu(AppServices.AuthService).Start();}}
