d51ecc397851ae2021adc17e1f38bf0eb8ce77c4
verona2024 <verona.ademaj@umib.net>
2026-03-24 23:31:56 +0100

    Add SessionManager class for user session management

---
+++ StudentJobPlatform/SessionManager.cs
@@ -0,0 +1,9 @@
+namespace StudentJobPlatform
+{
+    public static class SessionManager
+    {
+        public static int CurrentUserId = 0;
+        public static string CurrentUserRole = "";
+        public static string CurrentUserName = "";
+    }
+}
