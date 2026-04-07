9062b62abf331399afdcefa22f175bf5bf45b0a0
verona2024 <verona.ademaj@umib.net>
2026-04-07 21:02:13 +0200

    Update Student.cs

--- StudentJobPlatform/Models/Student.cs
+++ StudentJobPlatform/Models/Student.cs
@@ -5,8 +5,8 @@ namespace StudentJobPlatform.Models
         private string _major;
         private string _skills;
 
-        public string Major => _major;
-        public string Skills => _skills;
+        public new string Major => _major;
+        public new string Skills => _skills;
 
         public Student(int id, string name, string email, string password, string role,
                        string major, string skills)
