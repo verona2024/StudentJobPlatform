fb95b4f3a5ef15a09907a2f4e461668c03f7aaa0
verona2024 <verona.ademaj@umib.net>
2026-04-07 21:02:42 +0200

    Update User.cs

--- StudentJobPlatform/Models/User.cs
+++ StudentJobPlatform/Models/User.cs
@@ -2,43 +2,39 @@ namespace StudentJobPlatform.Models
 {
     public class User
     {
-        private int _id;
-        private string _name;
-        private string _email;
-        private string _password;
-        private string _role;
-        private string _major;
-        private string _skills;
-        private string _availability;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Major { get; set; }
+        public string Skills { get; set; }
+        public string Location { get; set; }
+        public string Availability { get; set; }
 
-        public int Id => _id;
-        public string Name => _name;
-        public string Email => _email;
-        public string Role => _role;
-        public string Major => _major;
-        public string Skills => _skills;
-        public string Availability => _availability;
+        public User()
+        {
+            Name = "";
+            Email = "";
+            Password = "";
+            Role = "";
+            Major = "";
+            Skills = "";
+            Location = "";
+            Availability = "";
+        }
 
         public User(int id, string name, string email, string password, string role)
         {
-            _id = id;
-            _name = name;
-            _email = email;
-            _password = password;
-            _role = role;
-            _major = "";
-            _skills = "";
-            _availability = "";
-        }
-
-        public bool CheckPassword(string password)
-        {
-            return _password == password;
-        }
-
-        public override string ToString()
-        {
-            return $"{_id},{_name},{_email},{_password},{_role},{_major},{_skills},{_availability}";
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            Role = role;
+            Major = "";
+            Skills = "";
+            Location = "";
+            Availability = "";
         }
     }
 }
