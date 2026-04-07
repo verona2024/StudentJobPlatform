5e1060a14331b706564525892f35ad1887da147a
verona2024 <verona.ademaj@umib.net>
2026-04-07 21:04:37 +0200

    Create Constants.cs

---
+++ StudentJobPlatform/Services/Constants.cs
@@ -0,0 +1,13 @@
+namespace StudentJobPlatform.Services
+{
+    public static class Constants
+    {
+        public const string StudentRole = "Student";
+        public const string EmployerRole = "Employer";
+        public const string AdminRole = "Admin";
+
+        public const string PendingStatus = "Pending";
+        public const string AcceptedStatus = "Accepted";
+        public const string RejectedStatus = "Rejected";
+    }
+}
